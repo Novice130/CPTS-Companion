03fd273e246ea8c4849efc607d1e37bfb04def35
Novice <learnnovice130@gmail.com>
2026-04-07 20:05:12 +0530

    Comment out Google Sign-In

--- auth.ts
+++ auth.ts
@@ -16,12 +16,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
+  /* socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     },
-  },
+  }, */
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24,     // refresh every 24h
